d06c3db494ef601ee3ad87ab9d739f179cbbc15f
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-01-19 19:21:40 -0800

    added the reference to the modules list

--- src/modules/mod.rs
+++ src/modules/mod.rs
@@ -2,8 +2,10 @@ mod aligner;
 mod settings;
 mod reference;
 mod pestats;
+mod build_reference;
 
 pub use self::settings::BwaSettings;
 pub use self::reference::BwaReference;
 pub use self::pestats::PairedEndStats;
 pub use self::aligner::BwaAligner;
+pub use self::build_reference::build_reference;
